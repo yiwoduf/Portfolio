6568c31a85fa7b5c4231f74dcf22b150214b8e01
leeable <37748514+yiwoduf@users.noreply.github.com>
2022-09-21 17:01:09 -0500

    HREF Buttons Activated
    Able to download resume.pdf
    Able to open github link
    Able to open instagram link

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,3 @@
-
 /*
 // CHECKING SCROLL POSITION
 window.onscroll = function (e) {
@@ -6,45 +5,33 @@ window.onscroll = function (e) {
 };
 */
 
+$(document).on("scroll", function () {
+  // FADE IN AND OUT EFFECT
+  if ($(document).scrollTop() > 2700 && $(document).scrollTop() < 4200) {
+    $(".badge-container").fadeIn();
+  } else {
+    $(".badge-container").fadeOut();
+  }
 
-$(document).on("scroll", function () { // FADE IN AND OUT EFFECT
-    if ($(document).scrollTop() > 2700 && $(document).scrollTop() < 4200)
-     {
-       $('.badge-container').fadeIn();
+  if ($(document).scrollTop() > 3900 && $(document).scrollTop() < 7500) {
+    $(".container-section-project-main-one").fadeIn(2000);
+  } else {
+    $(".container-section-project-main-one").fadeOut();
+  }
 
-     }
-     else {
-         $('.badge-container').fadeOut();
-     }
+  if ($(document).scrollTop() > 4500 && $(document).scrollTop() < 7500) {
+    $(".container-section-project-main-two").fadeIn(2000);
+    $(".container-section-project-main-three").fadeIn(2000);
+  } else {
+    $(".container-section-project-main-two").fadeOut();
+    $(".container-section-project-main-three").fadeOut();
+  }
 
-     if ($(document).scrollTop() > 3900 && $(document).scrollTop() < 7500)
-     {
-       $('.container-section-project-main-one').fadeIn(2000);
-
-     }
-     else {
-         $('.container-section-project-main-one').fadeOut();
-     }
-
-     if ($(document).scrollTop() > 4500 && $(document).scrollTop() < 7500)
-     {
-       $('.container-section-project-main-two').fadeIn(2000);
-       $('.container-section-project-main-three').fadeIn(2000);
-
-     }
-     else {
-         $('.container-section-project-main-two').fadeOut();
-         $('.container-section-project-main-three').fadeOut();
-     }
-
-     if ($(document).scrollTop() > 5800 && $(document).scrollTop() < 7500)
-     {
-        $('.container-section-project-main-four').fadeIn(2000);
-        $('.container-section-project-main-five').fadeIn(2000);
-
-     }
-     else {
-        $('.container-section-project-main-four').fadeOut();
-        $('.container-section-project-main-five').fadeOut();
-     }
+  if ($(document).scrollTop() > 5800 && $(document).scrollTop() < 7500) {
+    $(".container-section-project-main-four").fadeIn(2000);
+    $(".container-section-project-main-five").fadeIn(2000);
+  } else {
+    $(".container-section-project-main-four").fadeOut();
+    $(".container-section-project-main-five").fadeOut();
+  }
 });
